80d5b4b35b88555787ab9eccea624abcb1afb1c1
daxata <44601178+daxata@users.noreply.github.com>
2021-05-05 12:17:56 +0545

    Add files via upload

---
+++ categories.php
@@ -0,0 +1,73 @@
+<?php
+  include 'header.php';
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <title>Price Comparison</title>
+  </head>
+  <body>
+    <h1 style="align: center;">Categories</h1>
+
+    
+<div class="row">
+  <div class="col-sm-3">
+  <div class="card" style="width: 30rem; margin-left:9em;">
+  <img class="card-img-top" src="rice.jpg" style="align: justify; object-fit : cover;" alt="Card image cap">
+  <div class="card-body">
+    <h5><a href="rice.php" class="card-title">Rice and Rice Products</a></h5>
+  </div>
+    </div>
+  </div>
+  
+  <div class="col-sm-3">
+  <div class="card" style="width: 30rem; margin-left:15em;">
+  <img class="card-img-top" src="lentil.jpg" style="align: justify; object-fit : cover;" alt="Card image cap">
+  <div class="card-body">
+    <h5><a href="lentil.php" class="card-title">Beans and Lentils</a></h5>
+  </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+  <div class="card" style="width: 30rem; margin-left:21em;">
+  <img class="card-img-top" src="oil.jpg" style="align: justify; object-fit : cover;" alt="Card image cap">
+  <div class="card-body">
+    <h5><a href="oil.php" class="card-title">Oil and Vinegar</a></h5>
+  </div>
+    </div>
+  </div>
+  
+</div>
+
+<div class="row">
+  <div class="col-sm-3">
+  <div class="card" style="width: 30rem; margin-left:9em; margin-top:2em;">
+  <img class="card-img-top" src="spices.jpg" style="align: justify; object-fit : cover;" alt="Card image cap">
+  <div class="card-body">
+    <h5><a href="spice.php" class="card-title">Sugar and Spices</a></h5>
+  </div>
+    </div>
+  </div>
+  
+  <div class="col-sm-3">
+  <div class="card" style="width: 30rem; margin-left:15em; margin-top:2em;">
+  <img class="card-img-top" src="snacks.jpg" style="align: justify; object-fit : cover;" alt="Card image cap">
+  <div class="card-body">
+    <h5><a href="snacks.php" class="card-title">Snacks</a></h5>
+  </div>
+    </div>
+  </div>
+  
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ dbh.php
@@ -0,0 +1,7 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "Ashthalin_7";
+$dbname = "product_details";
+
+$conn = mysqli_connect($server, $username, $password, $dbname);
